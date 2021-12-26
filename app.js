cc3afeab289f6a194c0902d244b55741752c85c5
Marcus McBride <marcusmcb@gmail.com>
2021-12-26 14:49:37 -0800

    added express

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-import fetch from "node-fetch";
+import express from 'express'
+import fetch from 'node-fetch'
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
 
 let coinData = []
 
 const getCoins = async () => {
   try {
-    let req = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=volume_desc&per_page=99&page=1&sparkline=true');
-    let response = await req.json();
+    let req = await fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=volume_desc&per_page=99&page=1&sparkline=true'
+    )
+    let response = await req.json()
     // console.log("RESPONSE: ", response[1].id);
     for (let i = 0; i < response.length; i++) {
       coinData.push(response[i])
     }
-    console.log("COIN DATA: ", coinData[1])
+    console.log("DATA SAMPLE: ",coinData[1])
+    console.log('---------------------------------------------')
+    return coinData        
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-};
+}
 
-getCoins();
+app.get('/getcoins', async (req, res) => {
+   let response = await getCoins()
+   console.log(' --- API response returned --- ')
+   res.send(response)
+})
 
+app.listen(PORT, () => {
+  console.log('---------------------------------------------')
+  console.log(`*** Express App listening on PORT ${PORT} ***`)
+  console.log('---------------------------------------------')
+})
