c2c894479befd54c664b82b58871112a34fb392c
Marcus McBride <marcusmcb@gmail.com>
2021-12-26 17:03:11 -0800

    added mongo cluster connection, working

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 import express from 'express'
 import fetch from 'node-fetch'
+import colors from 'colors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
 
 const app = express()
-
 const PORT = process.env.PORT || 3000
+
+mongoose
+  .connect(process.env.MONGO_PROD_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('---------------------------------------------'.yellow)
+    console.log('*** MongoDB Cluster CONNECTED ***'.underline.magenta)
+    console.log('---------------------------------------------'.yellow)
+  })
+  .catch((err) => console.log(err))
 
 let coinData = []
 
@@ -13,26 +29,26 @@ const getCoins = async () => {
       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=volume_desc&per_page=99&page=1&sparkline=true'
     )
     let response = await req.json()
-    // console.log("RESPONSE: ", response[1].id);
     for (let i = 0; i < response.length; i++) {
       coinData.push(response[i])
     }
-    console.log("DATA SAMPLE: ",coinData[1])
-    console.log('---------------------------------------------')
-    return coinData        
+    console.log('MONGO KEY: ', process.env.MONGO_PROD_URI)
+    console.log('DATA SAMPLE BELOW:'.cyan)
+    console.log('---------------------------------------------'.yellow)
+    console.log(coinData[1])
+    console.log('---------------------------------------------'.yellow)
+    return coinData
   } catch (err) {
     console.log(err)
   }
 }
 
-app.get('/getcoins', async (req, res) => {
-   let response = await getCoins()
-   console.log(' --- API response returned --- ')
-   res.send(response)
+app.get('/', async (req, res) => {
+  getCoins().then((data) => res.send(data))
 })
 
 app.listen(PORT, () => {
-  console.log('---------------------------------------------')
-  console.log(`*** Express App listening on PORT ${PORT} ***`)
-  console.log('---------------------------------------------')
+  console.log('---------------------------------------------'.yellow)
+  console.log(`*** Express App listening on PORT ${PORT} ***`.cyan)
+  console.log('---------------------------------------------'.yellow)
 })
