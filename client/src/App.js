b865c9feb9373198d5e2bd3d75ff5d9bee42295f
Marcus McBride <marcusmcb@gmail.com>
2021-12-28 16:05:01 -0800

    cleanup boilerplate react code, data returning successfully from express endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,52 @@
+import React, { Fragment, useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => { 
+  
+  const [coinData, setCoinData] = useState({})
+  const [isBusy, setIsBusy] = useState(true)
+
+  useEffect(() => {
+    const getCoins = async () => {
+      try {
+        let req = await fetch("http://localhost:5000")
+        let response = await req.json()
+        return response
+      } catch (err) {
+        return err
+      }
+    }
+
+    getCoins().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+        setCoinData(data)
+        setIsBusy(false)
+      } else {
+        setCoinData(data)
+        setIsBusy(false)
+      }
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">            
+        <div>
+          {isBusy ? (
+            <p>Loading...</p>
+          ) : (
+            <Fragment>
+              <div>
+              <h1>Data</h1>
+              <p>Symbol: {coinData[1].symbol}</p>            
+              <p>Name: {coinData[1].name}</p>            
+              <p>Price: {coinData[1].current_price}</p>                          
+              </div>
+            </Fragment>          
+          )}
+        </div>
       </header>
     </div>
   );
